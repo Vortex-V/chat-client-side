89124566385afe66433e69fe81215273a0860985
Vortex-V <vitalik124124@mail.ru>
2022-02-28 19:42:51 +0300

    1. Вёрстка типов сообщений 3. Небольшие исправления

---
+++ assets/js/javascript.js
@@ -0,0 +1,276 @@
+$(() => {
+    const API = 'http://chat.api.click2mice.local',
+        POST = 'post',
+        GET = 'get'
+    let session = {
+        userId: null,
+        roomId: null,
+    }
+
+    function chatAjax(query, data = null, method = 'get') {
+        let options = {
+            method: method,
+            contentType: 'application/json',
+        };
+        if (data) options.data = JSON.stringify(data)
+        return $.ajax(API + query, options)
+            .fail((jqXHR) => {
+                console.log(jqXHR.responseJSON.errors)
+            });
+    }
+
+    let Chat = function () {
+
+        // FUNCTIONS
+
+        /**
+         * Осуществляет поиск и сохранение JQuery объектов элементов окна
+         * @param elements {{
+         *              name:'DOM object id',
+         *          }}
+         *
+         * @returns {{}} Список полученных элементов
+         */
+        this.elements = function (elements = null) {
+            if (elements) {
+                this.elements.list = {};
+                for (let name in elements) {
+                    let id = elements[name];
+                    let child = $('#' + id);
+                    child.length ? this.elements.list[name] = child : this.throwException(`Не найден элемент по id = ${id}`);
+                }
+            }
+            return this.elements.list
+        };
+
+        this.throwException = function (message) {
+            throw message;
+        };
+
+        this.toggleOpen = function () {
+            EL.chatWindow.toggleClass('closed');
+            this.checkIsClosed();
+        }
+
+        /**
+         * TODO подумать над тем, оставлять ли эту функцию или объединить с toggleOpen()
+         */
+        this.checkIsClosed = function () {
+            let chatWindow = EL.chatWindow;
+            if (chatWindow.hasClass('closed')) {
+                chatWindow.draggable('disable');
+            } else {
+                chatWindow.draggable('enable');
+                EL.messageTextArea.focus();
+            }
+        }
+
+        /**
+         * Разбивает текст на блоки по переносам строки
+         * @param text
+         * @returns {[]}
+         */
+        this.splitToDivs = function (text) {
+            let result = [];
+            for (let paragraph of text.split('\n')) {
+                result.push($('<div>').text(paragraph)[0]);
+            }
+            return result;
+        }
+
+        this.updateFieldHeight = function () {
+            // Определяет высоту текстового поля из количества введенных строк
+            EL.textAreaHeight.children()
+                .replaceWith(
+                    $('<div>').append(
+                        this.splitToDivs(
+                            EL.messageTextArea.val()
+                        )
+                    )
+                );
+            EL.messageTextArea.height(EL.textAreaHeight.height());
+        }
+
+        /**
+         * @param data {{}}
+         */
+        this.addMessageToField = function (data) {
+            EL.messagesList.children('.msgs-scrollable').prepend();
+        }
+
+        this.createMessageDiv = function () {
+
+        }
+
+        // REQUESTS
+        this.getRoom = function () {
+            let result = chatAjax(`/room/get?room_id=${session.roomId}`);
+            console.log(this.response);
+        }
+
+        this.getMessagesForRoom = function () {
+            let result = chatAjax(`/message/get?room_id=${1}`);
+            console.log(this.response);
+        }
+
+        this.createUser = function () {
+            let result = chatAjax(`/user/create`, {
+                display_name: 'TEST USER'
+            }, POST);
+            console.log(this.response);
+        }
+
+        this.createRoom = function () {
+            let result = chatAjax(`/room/create`, null, POST);
+            console.log(this.response);
+        }
+
+        this.addUserToRoom = function () {
+            let result = chatAjax(`/room/create`, {
+                room_id: 1,
+                user_id: 2
+            }, POST);
+            console.log(this.response);
+        }
+
+        this.syncRoomUsers = function () {
+            let result = chatAjax(`/room/create`, {
+                room_id: 1,
+            }, POST);
+            console.log(this.response);
+        }
+
+        this.sendMessage = async function () {
+            let messageBody = EL.messageTextArea.val();
+            if (messageBody) {
+                chatAjax('/message/send', {
+                    user_id: session.userId,
+                    room_id: session.roomId,
+                    body: messageBody
+                }, POST)
+                    .done((result) => {
+                        this.addMessageToField(result.data);
+                        EL.messageTextArea.height(25);
+                        EL.messageTextArea.val('');
+                    })
+                    .fail(() => {
+                        console.log('test')
+                    });
+            }
+        }
+        // END REQUESTS
+
+        // END FUNCTIONS
+
+
+        // ELEMENTS
+
+        const MESSAGE = `<div class="chat-message d-flex">
+                            <div class="message-left-col"></div>
+                            <div class="d-flex flex-column mx-1 message-center-col">
+                                <div class="d-flex align-items-end message-head"></div>
+                                <div class="mt-1 message-body formatted-message-text"></div>
+                            </div>
+                            <div class="d-flex flex-column justify-content-between message-right-col">
+                                <div class="message-me">Я</div>
+                                <div class="d-flex align-self-end message-timestamp">9:00</div>
+                            </div>
+                         </div>`;
+
+        /* <div class="chat-message d-flex">
+          <div class="message-left-col">
+            <img src="assets/files/users_default_avatars/User avatar.png" alt="User">
+          </div>
+          <div class="d-flex flex-column mx-1 message-center-col">
+            <div class="d-flex align-content-end message-head">
+            </div>
+            <div class="mt-1 message-body">
+            </div>
+          </div>
+          <div class="d-flex flex-column message-right-col">
+            <div class="d-flex align-self-end message-timestamp">9:00</div>
+          </div>
+        </div>*/
+
+        const EL = this.elements({
+            chatWindow: 'chat',
+            topPanel: 'chat-top-panel',
+            messageTextArea: 'chat-message-textarea',
+            textAreaHeight: 'chat-textarea-height',
+            messagesList: 'chat-messages-list',
+            buttonSendMessage: 'chat-send-message',
+        });
+
+        /* Открывает окно чата при нажатии Ctrl+Shift+ArrowUp
+        TODO: сделать комбинацию клавиш настриваемой */
+        $(window).keydown((e) => {
+            let keyEvent = e.originalEvent;
+            if (keyEvent.keyCode === 38 && keyEvent.shiftKey && keyEvent.ctrlKey) {
+                this.toggleOpen();
+            }
+        });
+
+        let allowClickOnTopPanel = EL.topPanel.click.allow = true;
+        EL.topPanel.click(() => {
+            if (allowClickOnTopPanel) {
+                this.toggleOpen();
+            }
+        });
+
+        EL.messageTextArea
+            .on('input', () => this.updateFieldHeight())
+            .keydown((e) => {  /* Отправляет сообщение на Ctrl+Enter TODO: сделать комбинацию клавиш настриваемой */
+                if (e.key === 'Enter' && e.ctrlKey) {
+                    this.sendMessage();
+                }
+            });
+
+        EL.buttonSendMessage.click(this.sendMessage);
+
+        /* TODO убрать полностью либо доработать */
+        /*EL.messagesList.scrollable = EL.messagesList.children('.msgs-scrollable');
+        EL.messagesList.on('wheel', (e) => {
+            let newScrollY = parseInt(EL.messagesList.scrollable.css('margin-top')) - e.originalEvent.deltaY * 2;
+            let absNewScrollY = Math.abs(newScrollY);
+            let maxScrollY = EL.messagesList.maxScrollY;
+            if (newScrollY <= 0 && absNewScrollY <= maxScrollY) {
+                if (absNewScrollY < 200) newScrollY = 0;
+                else if (absNewScrollY > maxScrollY - 200) newScrollY = -maxScrollY;
+                EL.messagesList.scrollable.css('margin-top', newScrollY);
+            }
+        });*/
+
+
+        EL.chatWindow
+            .draggable({
+                handle: EL.topPanel,
+                stack: this.chatWindow,
+                disabled: true,
+                start: function (event, ui) {
+                    allowClickOnTopPanel = false;
+                },
+                stop: function () {
+                    setTimeout(() => { // Не даёт произойти автоматическому закрытию окна сразу после перетаскивания
+                        allowClickOnTopPanel = true;
+                    }, 1);
+                }
+            })
+            .addClass('closed')
+            .show();
+    }
+
+    let chat = window.chat = new Chat();
+
+    /* Чтобы не стёртый ранее текст из поля ввода сообщения
+       влиял на высоту блока ввода после обновления страницы */
+    chat.updateFieldHeight();
+
+
+    // Dev tool
+    chat.startTest = function (userId = 1, roomId = 1) {
+        session.userId = userId;
+        session.roomId = roomId;
+        this.sendMessage();
+        return 1;
+    };
+});
